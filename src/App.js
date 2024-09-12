719dbd36cc239dda354ffad6978f0e3fda76d62e
AbhiMehetre <abhijit.m@arrivalsolutions.in>
2024-09-12 10:34:46 +0530

    Counter component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 
 import Header from "./components/Header/Header";
+import Counter from "./components/UI/Counter.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Counter />
     </div>
   );
 }
