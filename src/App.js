a195eee6cbffafb9e5a1693ef76a5b8a95c1fdb4
dikshagaikwad03 <gaikwaddiksha003@gmail.com>
2024-10-09 13:09:03 +0530

    Footer Component Completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState,useEffect} from "react";
 import "./App.css";
 
 import Header from "./components/Header/Header";
-import Hero from "./components/UI/Hero";
+import Hero from "./components/UI/Hero";    
 import Services from "./components/UI/Services";
 import About from "./components/UI/About";
 import Counter from "./components/UI/Counter.jsx";
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,7 +16,40 @@ const quickLinks01 = [
     },
 ]
 
+const quickLinks02 = [
+    {
+        path:'#',
+        display:'Pricing'
+    },
+    {
+        path:'#',
+        display:'Documentation'
+    },
+    {
+        path:'#',
+        display:'Guides'
+    },
+]
+
+const quickLinks03 = [
+    {
+        path:'#about',
+        display:'About'
+    },
+    {
+        path:'#',
+        display:'Jobs'
+    },
+    {
+        path:'#blog',
+        display:'Blog'
+    },
+]
+
 const Footer = () => {
+
+    const year = new Date().getFullYear()
+
   return <footer className="footer">
     <div className="container">
         <div className="footer__wrapper">
@@ -41,7 +74,34 @@ const Footer = () => {
                     ))}
                 </ul>
             </div>
+
+            <div className="footer__quick-links">
+                <h3 className="quick__links-title">Support</h3>
+                <ul className="quick__links">
+                    {quickLinks02.map((item,index) => (
+                        <li className="quick__link-item" key={index}>
+                            <a href={item.path}>{item.display}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="footer__quick-links">
+                <h3 className="quick__links-title">Company</h3>
+                <ul className="quick__links">
+                    {quickLinks03.map((item,index) => (
+                        <li className="quick__link-item" key={index}>
+                            <a href={item.path}>{item.display}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
+
+        <p className="copyright"> 
+            Copyright {year}, developed by YomIT. All rights 
+            reserved.{" "} 
+        </p>
     </div>
   </footer>
 }
